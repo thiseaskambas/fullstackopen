1f776e980d7095d7fdb189d66f410b0b28c4ef70
thiseaskambas <tkambas@yahoo.com>
2022-08-15 18:40:01 +0300

    end 1.13

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,21 +1,34 @@
 import { useState } from "react";
 
-const App = () => {
-  const anecdotes = [
-    "If it hurts, do it more often.",
-    "Adding manpower to a late software project makes it later!",
-    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
-    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
-    "Premature optimization is the root of all evil.",
-    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
-    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
-  ];
+const anecdotes = [
+  "If it hurts, do it more often.",
+  "Adding manpower to a late software project makes it later!",
+  "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+  "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+  "Premature optimization is the root of all evil.",
+  "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+  "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+];
 
+const App = () => {
   const [selected, setSelected] = useState(0);
 
+  const [points, setPoints] = useState([]);
+  const voteHandler = () => {
+    const newPoints = [...points];
+    newPoints[selected] === undefined
+      ? (newPoints[selected] = 1)
+      : (newPoints[selected] += 1);
+    setPoints(newPoints);
+  };
   return (
     <>
       <div>{anecdotes[selected]}</div>
+      <p>
+        This joke has {points[selected] === undefined ? "0" : points[selected]}
+        points.
+      </p>
+      <button onClick={voteHandler}>vote</button>
       <button
         onClick={() => {
           setSelected(Math.floor(Math.random() * anecdotes.length));
